6a834eb72c829bddf3b8f6a58de4e781b26d0b09
Daniel García Moreno <danigm@wadobo.com>
2017-01-14 21:32:06 +0100

    Using arg to tell the program what problem to resolve

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().count() != 2 {
+        println!("Introduce the problem number");
+        return;
+    }
+
+    let arg = env::args().nth(1).unwrap();
+    let argn: i32 = arg.parse().ok().unwrap_or(1);
+
+    match argn {
+        x @ _ => println!("Problem {} not implemented", x),
+    }
 }
