523bcaf57d13d7fc407ed254e82e72e0cf46485c
Daniel García Moreno <danigm@wadobo.com>
2017-01-15 00:07:37 +0100

    First problem solved

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -1,4 +1,8 @@
+extern crate problems;
+
 use std::env;
+
+use problems::p1;
 
 fn main() {
     if env::args().count() != 2 {
@@ -10,6 +14,7 @@ fn main() {
     let argn: i32 = arg.parse().ok().unwrap_or(1);
 
     match argn {
+        1 => p1::solve(),
         x @ _ => println!("Problem {} not implemented", x),
     }
 }
---
+++ euler/src/p1.rs
@@ -0,0 +1,30 @@
+//! Problem 1
+//! Multiples of 3 and 5
+//!
+//! If we list all the natural numbers below 10 that are multiples of 3 or
+//! 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the
+//! sum of all the multiples of 3 or 5 below 1000.
+
+
+/// Returns the sum of 3 and 5 multiples below `n`
+///
+/// # Examples
+///
+/// ```
+/// extern crate problems;
+///
+/// fn main() {
+///     assert_eq!(233168, problems::p1::multiples(1000));
+/// }
+/// ```
+pub fn multiples(n: i32) -> i32 {
+    let sol = (1..n)
+        .filter(|&x| x % 3 == 0 || x % 5 == 0)
+        .fold(0, |sum, x| sum + x);
+    sol
+}
+
+/// Solves the problem for 1000
+pub fn solve() {
+    println!("Solution: {}", multiples(1000));
+}
